eb53073aec38b8ccf4f6abdc4ff144a964bbc1d8
unknown <wallacetashyn@gmail.com>
2022-12-30 15:10:46 -0500

    completed the sign up page implementation

--- lib/routes/signup.dart
+++ lib/routes/signup.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
 import 'package:appointment_app_mobile/routes/home.dart';
 import 'package:appointment_app_mobile/routes/login.dart';
+import 'package:appointment_app_mobile/utils/apiService.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -12,6 +14,14 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  //text input controllers
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,88 +42,175 @@ class _SignUpPageState extends State<SignUpPage> {
                 SizedBox(
                   height: 45,
                 ),
-                Text(
-                  "First Name",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextField(
-              
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                Text(
-                  "Last Name",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextField(
-              
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                Text(
-                  "Email Address",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextField(
-              
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                Text(
-                  "Password",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    suffix: Icon(
-                      Icons.remove_red_eye,
-                      size: 16,
-                      color: Color(0xff8696BB),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                Text(
-                  "Confirm Password",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    suffix: Icon(
-                      Icons.remove_red_eye,
-                      size: 16,
-                      color: Color(0xff8696BB),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 50,),
-                Container(
-                  height: 65,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Color(0xff4894FE),
-                    borderRadius: BorderRadius.all(Radius.circular(25))
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "First Name",
+                        style: Theme.of(context).textTheme.headline5,
                       ),
-                    ),
+                      TextFormField(
+                        controller: firstNameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Text(
+                        "Last Name",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      TextFormField(
+                        controller: lastNameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Text(
+                        "Email Address",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+                          final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+                          if (!regex.hasMatch(value)){
+                            return 'This field must be an email';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Text(
+                        "Password",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                          suffix: Icon(
+                            Icons.remove_red_eye,
+                            size: 16,
+                            color: Color(0xff8696BB),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+                          RegExp regex = RegExp(r'^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$');
+                          //validate password
+                          if(!regex.hasMatch(value)){
+                            return 'Invalid password. include a minimum of eight characters, at least one letter, number and a special character.';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Text(
+                        "Confirm Password",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      TextFormField(
+                        controller: confirmPasswordController,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                          suffix: Icon(
+                            Icons.remove_red_eye,
+                            size: 16,
+                            color: Color(0xff8696BB),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required';
+                          }
+                          else if (value != passwordController.text){
+                            return 'The password does not match';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 50,),
+                      InkWell(
+                        onTap: () async{
+                          try{
+                            if(_formKey.currentState!.validate()) {
+                              await ApiService.createUser(
+                                emailController.text.trim(),
+                                firstNameController.text.trim(),
+                                lastNameController.text.trim(),
+                                passwordController.text.trim()
+                              );
+                              //sign in user
+                              await FirebaseAuth.instance.signInWithEmailAndPassword(
+                                email: emailController.text.trim(),
+                                password: passwordController.text.trim(),
+                              );
+
+                              //push to home page
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Account successfully created')),
+                              );
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) { return HomePage();},));
+                            }
+                          } catch(e){
+                            if(e.toString().contains("The email address is already in use")){
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('This email address is already taken.')),
+                              );
+                              return;
+                            }
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Error occured. Try again')),
+                            );
+                          }
+                        },
+                        child: Container(
+                          height: 65,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            color: Color(0xff4894FE),
+                            borderRadius: BorderRadius.all(Radius.circular(25))
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 SizedBox(height: 30,),
---
+++ lib/utils/apiService.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: file_names, prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uuid/uuid.dart';
+
+class ApiService {
+  const ApiService();
+
+  static Future<void> createUser(String email, String firstName, String lastName, String password ) async {
+    //creating firebase Auth record
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password
+    );
+
+    //creating firestore user record
+    final uuid = Uuid();
+    final String userId = uuid.v1();
+    
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    await users.add({
+      'userId': userId,
+      'email': email,
+      'firstName': firstName,
+      'lastName': lastName,
+      'role': "Patient"
+    });
+  }
+
+  static Future<void> getUser() async {
+    return;
+  }
+
+  static Future<void> deleteUser() async {
+    return;
+  }
+  
+}
